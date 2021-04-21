541c77e0320cff947c1b3428a21cfdd910bd9276
Jhonathas Matos <jhonathas@gmail.com>
2021-04-21 16:26:40 -0300

    remove enviroment configs

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,13 @@
 use Mix.Config
 
-import_config "#{Mix.env()}.exs"
+config :skeleton_service, ecto_repos: [Skeleton.App.Repo]
+
+config :skeleton_service, Skeleton.App.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  pool: Ecto.Adapters.SQL.Sandbox,
+  hostname: System.get_env("POSTGRES_HOST", "localhost"),
+  database: "skeleton_service_test",
+  password: System.get_env("POSTGRES_PASSWORD", "123456"),
+  username: System.get_env("POSTGRES_USERNAME", "postgres")
+
+config :logger, :console, level: :error
